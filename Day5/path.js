11d4f7e1ff885a8a3a71c96d7bd5755ea012d205
mstars_lab2_01 <mstars_lab2_01@mstars-lab2-01s-iMac.local>
2022-04-20 15:19:40 +0800

    added path.js file in Day5 folder

---
+++ Day5/path.js
@@ -0,0 +1,42 @@
+const bye = {
+  name: "Javkhlan",
+  message: "Bye",
+};
+var http = require("http");
+var queryString = require("querystring");
+
+http
+  .createServer((request, response) => {
+    console.log(request.url);
+
+    if (request.url === "/hello") {
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.write("<h1>Hello Javkhlan</h1>");
+      response.end();
+    } else if (request.url === "/bye") {
+    } else if (request.url.match(/^\/q/)) {
+      const value = request.url.split("=");
+      response.writeHead(200, { "Content-Type": "application/json" });
+      if (value[1] == "why") {
+        response.write("<h1>Does not matter</h1>");
+      } else if (value[1] == "hi") {
+        response.write("<h1>Bye</h1>");
+      } else {
+        response.write("<h1>Mori</h1>");
+      }
+      response.end();
+    } else if (request.url.match(/^\/echo/)) {
+      return respondEcho(request, response);
+    } else {
+      response.writeHead(404, { "Content-Type": "text/plain" });
+      response.write("Not Found");
+      response.end();
+    }
+  })
+  .listen(3000);
+console.log("Server is starting at 3000");
+
+function respondEcho(req, res) {
+  console.log(req.url);
+  res.end();
+}
